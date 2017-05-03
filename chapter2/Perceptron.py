d05ef5af6edf854e5c0e51fb1588bf058073f0e8
KimSewon <swkim@KimSewonui-MacBook-Pro.local>
2017-05-03 23:22:37 +0900

    Initial version document added

--- chapter2/Perceptron.py
+++ chapter2/Perceptron.py
@@ -6,3 +6,11 @@ Created on Wed May  3 22:51:54 2017
 @author: swkim
 """
 
+def AND(x1, x2):
+    w1, w2, theta = 0.5, 0.5, 0.7
+    tmp = x1*w1 + x2*w2
+    if tmp <= theta:
+        return 0
+    elif tmp > theta:
+        return 1
+    
