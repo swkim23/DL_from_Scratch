d8d4b73342fdf8dfc58862508d5e6b713cac4bd4
KimSewon <swkim@KimSewonui-MacBook-Pro.local>
2017-05-04 10:56:15 +0900

    Implements Perceptions using NP Array.

--- chapter2/Perceptron.py
+++ chapter2/Perceptron.py
@@ -6,11 +6,40 @@ Created on Wed May  3 22:51:54 2017
 @author: swkim
 """
 
-def AND(x1, x2):
+import numpy as np
+
+def simpleAND(x1, x2):
     w1, w2, theta = 0.5, 0.5, 0.7
     tmp = x1*w1 + x2*w2
     if tmp <= theta:
         return 0
     elif tmp > theta:
         return 1
-    
+
+def simpleOR(x1, x2):
+    w1, w2, theta = 0.5, 0.5, 0.3
+    tmp =  x1*w1 + x2*w2
+    if tmp <= theta:
+        return 0
+    elif tmp > theta:
+        return 1
+        
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5,0.5])
+    b = -0.7
+    tmp = np.sum(w*x)+b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w*x)+b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
